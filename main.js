5a91cf8ba37dd51baa63ce9d6872fc644df49790
iamdokun <dokun.abraham@gmail.com>
2020-09-10 03:25:46 +0100

    I added authentication to the login page and registration page

--- main.js
+++ main.js
@@ -37,11 +37,92 @@ function loginReg() {
 // Login Page Authentication
 
 let loginBtn = document.querySelector('#loginButton');
-let regButton = document.querySelector('#btnReg2');
+const userCheck = document.querySelector('#userchk');
+const userName = document.querySelector('#usernameLogin');
+const passCheck = document.querySelector('#passchk');
+const passWord = document.querySelector('#passwordLogin');
 
-loginBtn.addEventListener('click', loginFunction)
+userName.addEventListener('keyup', userData);
+passWord.addEventListener('keyup', passData);
+loginBtn.addEventListener('click', loginFunction);
 
 function loginFunction() {
-    const userCheck = document.querySelector('#userchk')
     
+    if (userName.value == "") {
+        userCheck.style.display = 'block';
+        
+    } else if (passWord.value == "") {
+        passCheck.style.display = 'block';
+    } else {
+        userCheck.style.display = 'none';
+        passCheck.style.display = 'none';
+    }   
+}
+
+function userData() {
+    userCheck.style.display = 'none';
+}
+function passData() {
+    passCheck.style.display = 'none';
+}
+
+// Registration Page Authentication
+let regButton = document.querySelector('#btnReg2');
+const fnameInp = document.querySelector('#fnameId');
+const fnameCheck = document.querySelector('#fnamechk');
+const lnameInp = document.querySelector('#lnameId');
+const lnameCheck = document.querySelector('#lnamechk');
+const emailInp = document.querySelector('#emailId');
+const emailCheck = document.querySelector('#emailchk');
+const regPassInp = document.querySelector('#passwordId');
+const regPassCheck = document.querySelector('#regpasschk');
+const regCpassInp = document.querySelector('#cpassId');
+const regCpassCheck = document.querySelector('#cpasschk');
+
+regButton.addEventListener('click', regFunction);
+fnameInp.addEventListener('keyup', fnameData);
+lnameInp.addEventListener('keyup', lnameData);
+emailInp.addEventListener('keyup', emailData);
+regPassInp.addEventListener('keyup', regPassData);
+regCpassInp.addEventListener('keyup', regCpassData);
+
+function regFunction() {
+    if (fnameInp.value == "") {
+        fnameCheck.style.display = 'block';
+        
+    } else if (lnameInp.value == "") {
+        lnameCheck.style.display = 'block';
+
+    } else if (emailInp.value == "") {
+        emailCheck.style.display = 'block';
+
+    } else if (regPassInp.value == "") {
+        regPassCheck.style.display = 'block';
+
+    } else if (regCpassInp.value == "") {
+        regCpassCheck.style.display = 'block';
+
+    } else {
+        fnameCheck.style.display = 'none';
+        lnameCheck.style.display = 'none';
+        emailCheck.style.display = 'none';
+        regPassCheck.style.display = 'none';
+        regCpassCheck.style.display = 'none';
+    }
+}
+
+function fnameData() {
+    fnameCheck.style.display = 'none';
+}
+function lnameData() {
+    lnameCheck.style.display = 'none';
+}
+function emailData() {
+    emailCheck.style.display = 'none';
+}
+function regPassData() {
+    regPassCheck.style.display = 'none';
+}
+function regCpassData() {
+    regCpassCheck.style.display = 'none';
 }
